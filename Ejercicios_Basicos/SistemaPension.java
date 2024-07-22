ce6b9e5da2a56e33a5981567b6325f5e20bed2ba
Josue <josueporras124@gmail.com>
2024-07-22 16:20:54 -0600

    Update Sistema Pension

--- Ejercicios_Basicos/SistemaPension.java
+++ Ejercicios_Basicos/SistemaPension.java
@@ -9,3 +9,71 @@ Por cada año adicional después de los 25, se añade un 1% extra, hasta un máx
 Muestra si el empleado es elegible, y si lo es, cuál sería su pensión.
  *
  * */
+
+import java.util.Scanner;
+
+class Datos{
+    private int JubiladoEdad;
+    private int ServicioOld;
+    private int UltimoSalario;
+    public Scanner in;
+    
+    public Datos(){
+        in = new Scanner(System.in);
+    }
+    
+    public void IngresoDatos(){
+        System.out.println("Ingrese la Edad del Participante a Jubilar :");
+        JubiladoEdad = in.nextInt();
+        System.out.println("Ingrese Anos de Servicio :");
+        ServicioOld = in.nextInt();
+        System.out.println("Ingrese El Ultimo Salario :");
+        UltimoSalario = in.nextInt();
+        in.nextLine();
+    }
+    public int GetEdad(){
+        return JubiladoEdad;
+    }
+    public int GetServicio(){
+        return  ServicioOld ;
+    }
+    public int GetUltiSalario(){
+        return UltimoSalario;
+    }
+    
+}
+
+//sub class
+class CalcularJubilacion extends Datos{
+    public int EdadMinima;
+    public int PensionServicio;//minimo 25 anos
+    public int PensionBase;
+    public float EdadPensionado;
+    public int ServicioPensionado;
+    public int UltimoSalarioPensionado;
+    
+    public CalcularJubilacion(){
+        super();
+        EdadMinima = 65;
+        PensionServicio = 25;
+    }
+    public void miPension(){
+        EdadPensionado = GetEdad();
+        ServicioPensionado = GetServicio();
+        UltimoSalarioPensionado = GetUltiSalario();
+        
+        if(EdadPensionado != EdadMinima && ServicioPensionado != PensionServicio){
+            System.out.println("No con los requisitos minimos");
+        }else if{
+            
+        }
+    }
+        
+}
+
+public class SistemaPension{
+    public static void main(String[] args){
+        Datos datosJubilado = new Datos();
+        datosJubilado.IngresoDatos();
+    }
+}
