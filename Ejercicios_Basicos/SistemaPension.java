865c2440ceb1752c6ec482bc7e3812578be622a5
Josue <josueporras124@gmail.com>
2024-07-22 14:45:21 -0600

    Sistema de pensiones

---
+++ Ejercicios_Basicos/SistemaPension.java
@@ -0,0 +1,11 @@
+/*
+ *Sistema de jubilación:
+Crea un programa que determine si un empleado es elegible para jubilación y calcule su pensión.
+
+La edad mínima para jubilarse es 65 años.
+Se necesitan al menos 25 años de servicio para una pensión completa.
+La pensión base es el 70% del último salario.
+Por cada año adicional después de los 25, se añade un 1% extra, hasta un máximo del 90%.
+Muestra si el empleado es elegible, y si lo es, cuál sería su pensión.
+ *
+ * */
