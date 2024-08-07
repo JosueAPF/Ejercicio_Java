573d46e9ac1d02945c6f0237efeae990c367a4eb
josue <josueporras124@gmail.com>
2024-08-07 14:29:28 -0600

    Implementacion de arrays para almacenar los datos de Factura y Productos ahora pide hasta n productos para una factura

--- MIniProyectos/1/ProyectoFactura.java
+++ MIniProyectos/1/ProyectoFactura.java
@@ -1,15 +1,18 @@
 //utilizando composición
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class Factura{
 
    private String Nit,Cliente,Fecha; //porq que string? para usar numeros y letras
+   private ArrayList<Productos> productos;//para que cada factura puede tener mas de un producto
    
    public Factura(){
       Nit = "";
       Fecha = "";
       Cliente =""; 
+      productos = new ArrayList<>();
    } 
    //Metodos Setter() insertar los datos mas adelant
    public String setNit(String nit){
@@ -20,6 +23,21 @@ class Factura{
    }
    public String setFecha(String fh){
         return this.Fecha=fh;
+   }
+   //metodos para agregarle a el array los productos
+   public void agregarProducto(Productos producto){
+      productos.add(producto);
+   }
+   //mostrara los elementos del array
+   public ArrayList<Productos> getProductos(){
+         return productos;
+   }
+   
+   @Override
+   public String toString(){
+      return "\n\t\t\t"+"Nit :"+Nit+
+             "\n\t\t\t"+"Cliente :"+Cliente+
+             "\n\t\t\t"+"Fecha :"+Fecha;                                   
    }
   
 }
@@ -58,21 +76,38 @@ class Productos{
     public double getPresioUnitario(){
         return PrecioUnit;
     }
+    public double SubTotalProducto(){
+         return PrecioUnit*Cantidad;
+    }
+    public double ImpuestoProducto(){
+         double iva = 0.12;
+         return PrecioUnit*iva;
+    }
+    public double Total(){
+         //subtotal_con_impuesto_producto = (precio_unitario + impuesto_producto) * cantidad
+         double impuestoAplicado = ImpuestoProducto();
+         return (PrecioUnit+impuestoAplicado)*Cantidad;
+    }
+    @Override
+    public String toString(){
+      return  "\n"+"Codigo :"+Codigo+
+              "\n"+"Producto :"+Producto+
+              "\n"+"Cantidad :"+Cantidad+                                   
+              "\n"+"Precio Unitario :"+PrecioUnit;   
+    }
 }
 
-//falta logica de procediminetos agregar el array como rayos ingresar los datos si los tengo por separados
-//probar usar toString
 
 class adminFacturas{
     //instancia de las clases 
     private Factura myFactura;
-    private Productos myProducts;
     //control del ciclo while y salida del menu
     private boolean Opcion;
     private int opcSalir;
-    private int opcData;
     //instancia de Scanner
-    private Scanner in;    
+    private Scanner in;  
+    //instancia del  ArrayList de tipo Object para Captura cualquier Tipo de Dato
+    private ArrayList<Object> ListaFactura; 
    
    
    public adminFacturas(){
@@ -80,9 +115,16 @@ class adminFacturas{
        opcSalir=1;
        in = new Scanner(System.in);
        myFactura = new Factura();
-       myProducts = new Productos();
+       ListaFactura = new ArrayList<>();
+   }
+   public void MostrarTitulo(){
+         System.out.println("\n========Systema de Facturacion========");
+         System.out.println("=====Josue Abraham Porras Figuero=====");
+         System.out.println("================Menu=======================");
    }
    public void IngresoDatos(){
+       //para que se ingrese mas de un producto
+       char continuar;
        
        //datos de ingreso para la class Factura
        System.out.println("Ingrese Nit :");
@@ -91,76 +133,102 @@ class adminFacturas{
        myFactura.setCliente(in.nextLine());
        System.out.println("Ingrese Fecha :");
        myFactura.setFecha(in.nextLine());
-       
-       //datos de ingreso para la class Producto
-       System.out.println("Ingrese Codigo :");
-       myProducts.setCodigo(in.nextLine());
-       System.out.println("Ingrese Producto :");
-       myProducts.setProducto(in.nextLine());
-       System.out.println("Ingrese Cantidad :");
-       myProducts.setCantidad(in.nextInt());
-       System.out.println("Ingrese precio por Unidad :");
-       myProducts.setPreicoUnitario(in.nextDouble());
-       
-   } 
+       ListaFactura.add(myFactura);
+             
+      
+       do{
+         Productos myProducts = new Productos();
+        //datos de ingreso para la class Producto
+         System.out.println("Ingrese Codigo :");
+         myProducts.setCodigo(in.nextLine());
+         System.out.println("Ingrese Producto :");
+         myProducts.setProducto(in.nextLine());
+         System.out.println("Ingrese Cantidad :");
+         myProducts.setCantidad(in.nextInt());
+         in.nextLine();
+         System.out.println("Ingrese precio por Unidad :");
+         myProducts.setPreicoUnitario(in.nextDouble());
+         in.nextLine();
+         myFactura.agregarProducto(myProducts);
+         
+         
+         System.out.println("Desea Continuar :(S/N):");
+         continuar = in.nextLine().charAt(0);
+       }while(continuar == 's');
+   }
    public void Procedimientos(){
-       double Punitario = myProducts.getPresioUnitario();
-       int CantidadP = myProducts.getCantidad();
-       double SubTotal = Punitario*CantidadP;
-       double impuestoAplicado = SubTotal*0.12;
-           double TotalPagar = (SubTotal + impuestoAplicado);
-               
-               
-               System.out.println("Subtotal: " + SubTotal);
-       System.out.println("Impuesto aplicado: " + impuestoAplicado);
-       System.out.println("Total a Pagar: " + TotalPagar);
-       
-       
+         double subtotal = 0;
+         double totalImpuestos = 0;
+         double totalPagar = 0;
+         
+         
+        for (int i = 0; i < myFactura.getProductos().size(); i++) {
+            Productos producto = myFactura.getProductos().get(i);//pararecupera un objeto Productos del ArrayList<Productos> 
+            subtotal += producto.SubTotalProducto();
+            totalImpuestos += producto.ImpuestoProducto() * producto.getCantidad();
+            totalPagar += producto.Total();
+        }
+        //totales acomulados
+        System.out.println("\nSubtotal: " + subtotal);
+        System.out.println("Total Impuestos: " + totalImpuestos);
+        System.out.println("Total a Pagar: " + totalPagar);
+   }
+   public void Factura(){     
+        System.out.println("Datos de la Factura");
+        for(int i=0;i<ListaFactura.size();i++){
+            System.out.println("Factura no :"+(i+1));
+            System.out.println(ListaFactura.get(i));
+        }
+        System.out.println("Datos Producto");
+        for (int i = 0; i < myFactura.getProductos().size(); i++) {
+             System.out.println(myFactura.getProductos().get(i));
+         }
+        
+        
    }
    public void MenuFacturas(){
        
        while(Opcion){
-            System.out.println("-----Menu----");
-            System.out.println("1 Ingreso Datos de Facturacion ");
-            System.out.println("2 Mostrar Datos de Facturacion");
-            System.out.println("3 Salir");
+            MostrarTitulo();
+            System.out.println("1. Nueva Factura Factura ");
+            System.out.println("2. Mostrar factura");
+            System.out.println("3. Calcular totales");
+            System.out.println("4 Salir\n");
+            System.out.print("Seleccione una opcion: ");
             opcSalir = in.nextInt();
+            in.nextLine();
+            System.out.print("\n");
            
            switch(opcSalir){
                case 1:
-                    System.out.println("INGRESO DE DATOS");
-                break;
+                    System.out.println("Ingreso Datos Factura Y Producto");
+                    IngresoDatos();
+                    break;
                case 2:
-                    System.out.println("MOSTRAR LOS DATOS");
+                    System.out.println("Mostrar factura");
+                    Factura();
                 break;
-               case 3:
-                    System.out.println("Salida del Programa");
-                break;
+                case 3:
+                     System.out.println("Calcular totales");
+                     Procedimientos();
+                     break;
+                 case 4:
+                     System.out.println("Salida");
+                     Opcion = false;
+                     break;  
                default:
                     System.out.println("Opcion no valida");
-                break;
+                    break;
            }
-           System.out.println("Desea seguir ingresando datos si:1 no:0");
-           opcData = in.nextInt();
-           
-           if(opcData !=1){
-               System.out.println("Salir");
-           }
-       }
-      
+      }  
    }
-   
-
 }
 
 public class ProyectoFactura{
-	public static void main(String[] args){
-		      
+	public static void main(String[] args){    
       System.out.println("\n");
       adminFacturas myGestor = new adminFacturas();
-	      myGestor.IngresoDatos();
-	      myGestor.Procedimientos();
-	      //myGestor.Procedimientos();
+	   myGestor.MenuFacturas();        
 	}
 }
 
